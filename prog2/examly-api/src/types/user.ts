f5ee18ecdf0ae0fd0840e3c1050bb481cc2b6d98
Gabriel Santos Costa <gabrielsantoscosta005@gmail.com>
2025-07-21 12:58:29 -0300

    use nest

--- prog2/examly-api/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: string;
-  email: string;
-  password_hash: string;
-  name: string;
-  role: 'student' | 'teacher' | 'admin';
-  created_at: Date;
-  updated_at: Date;
-}
